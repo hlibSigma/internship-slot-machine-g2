67f77e596aad949297057fd0d41b89a8110098bc
hlib.savenko <hlib.savenko@sigma.software>
2022-12-06 20:50:00 +0200

    removing of pixi v5 package.

--- pixiv5/build/assets.js
+++
@@ -1,5 +0,0 @@
-var texturepackerify = require("texturepackerify");
-
-texturepackerify.pack({ url: "./src/assets/atlases/", hashUrl: "./assets/", force: false }, () => {
-    console.log("assets built");
-});
--- pixiv5/src/ts/layoutManager/LayoutManager.ts
+++
@@ -1,6 +0,0 @@
-import Signal from "../helpers/signals/signal/Signal";
-
-export default class LayoutManager {
-    static UPDATE:Signal<any> = new Signal<any>();
-
-}
