1ae574ba768200ae8e28e4ac3fcd4a51b7e15042
hlib.savenko <hlib.savenko@sigma.software>
2022-12-08 19:53:07 +0200

    [Howler] changes for 2.8. Libraries and frameworks lecture changes;

--- pixiv6/src/dev/DevController.ts
+++ pixiv6/src/dev/DevController.ts
@@ -7,33 +7,17 @@ import Stats from "stats.js";
 import DevPixiDrawLayoutPlugin from "app/layoutManager/DevPixiDrawLayoutPlugin";
 import dependencyManager from "app/model/injection/InjectDecorator";
 import LayoutManager from "app/layoutManager/LayoutManager";
+import DevToolUtils from "app/dev/DevToolUtils";
 
 export default class DevController {
-    private stats:Stats = new Stats();
+    private stats: Stats = new Stats();
 
     constructor() {
         this.setupStats();
         const gui = new GUI();
         this.setupGeneralHooks(gui);
-        let target = {
-            left:()=>{
-                console.log("left");
-                gameModel.updateOrientation.emit("left");
-            },
-            right:()=>{
-                console.log("right");
-                gameModel.updateOrientation.emit("right");
-            }
-        };
-        gui.add(target, "left");
-        gui.add(target, "right");
-        gui.add({speedFactor:3}, "speedFactor").onChange(value => {
-            console.log("value", value);
-            gameModel.setSpeedFactor.emit(value);
-        });
-        gui.add({run:()=>{
-            gameModel.devAnimation.run.emit();
-        }}, "run");
+        this.setupPacmanHooks(gui);
+        this.setupSounds(gui);
         gui.add({
             gameLayoutTest: () => {
                 gameModel.updateLayout.emit({
@@ -58,7 +42,7 @@ export default class DevController {
             }, "stats visibility");
             hotKeyTool.registerOnKey("2", () => {
                 let layoutManager = dependencyManager.resolve(LayoutManager);
-                if(layoutManager.hasPlugin(devPixiDrawLayoutPlugin)){
+                if (layoutManager.hasPlugin(devPixiDrawLayoutPlugin)) {
                     layoutManager.removePlugin(devPixiDrawLayoutPlugin);
                 } else {
                     layoutManager.addPlugin(devPixiDrawLayoutPlugin);
@@ -67,7 +51,32 @@ export default class DevController {
         }, "dev tools");
     }
 
-    private setupGeneralHooks(gui:GUI) {
+    private setupPacmanHooks(gui: GUI) {
+        let guiPacman = gui.addFolder("packman");
+        let target = {
+            left: () => {
+                console.log("left");
+                gameModel.updateOrientation.emit("left");
+            },
+            right: () => {
+                console.log("right");
+                gameModel.updateOrientation.emit("right");
+            }
+        };
+        guiPacman.add(target, "left");
+        guiPacman.add(target, "right");
+        guiPacman.add({speedFactor: 3}, "speedFactor").onChange(value => {
+            console.log("value", value);
+            gameModel.setSpeedFactor.emit(value);
+        });
+        guiPacman.add({
+            run: () => {
+                gameModel.devAnimation.run.emit();
+            }
+        }, "run");
+    }
+
+    private setupGeneralHooks(gui: GUI) {
         let generalGui = gui.addFolder("general");
         generalGui.add({
             play: () => {
@@ -88,5 +97,59 @@ export default class DevController {
         Main.APP.ticker.add(dt => {
             stats.update();
         });
+    }
+
+    private setupSounds(gui: GUI) {
+        let howler = gameModel.getHowler();
+        let id: number;
+        let target = {
+            rate: 1,
+            _rateMin: -1,
+            _rateMax: 10,
+            _rateStep: 0.01,
+            _rateUpdate: (data:any)=>{
+                console.log(data);
+                howler.rate(data, id);
+            },
+            volume: 1,
+            _volumeMin: 0,
+            _volumeMax: 1,
+            _volumeStep: 0.01,
+            _volumeUpdate: (data:any)=>{
+                console.log(data);
+                howler.volume(data);
+            },
+            soundId: "success",
+            _soundIdItems: ["success", "relax_loop"],
+            play: () => {
+                id = howler.play(target.soundId);
+            },
+            stop: () => {
+                howler.stop(id);
+            },
+            unPause: () => {
+                howler.play(id);
+            },
+            mute: () => {
+                howler.mute(true);
+            },
+            unMute: () => {
+                howler.mute(false);
+            },
+            fadeIn: () => {
+                id = howler.play(target.soundId);
+                howler.fade(0, 1, 3000, id);
+            },
+            fadeOut: () => {
+                id = howler.play(target.soundId);
+                howler.fade(1, 0, 3000, id);
+            },
+            fade: () => {
+                howler.fade(1, 0, 1500, id);
+                id = howler.play(target.soundId);
+                howler.fade(0, 1, 3000, id);
+            },
+        };
+        DevToolUtils.setupObj(target, "sounds", gui);
     }
 }
