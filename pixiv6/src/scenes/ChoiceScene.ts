2588a52107ee4822af113c33103daffaa093f5bc
hlib.savenko <hlib.savenko@sigma.software>
2022-12-15 01:24:46 +0200

    #13 | font correction on choice scene;

--- pixiv6/src/scenes/ChoiceScene.ts
+++ pixiv6/src/scenes/ChoiceScene.ts
@@ -11,12 +11,12 @@ import TextButtonControl from "app/controls/button/TextButtonControl";
 import StrictResourcesHelper from "app/pixi/StrictResourcesHelper";
 import ButtonControl from "app/controls/button/ButtonControl";
 import FragShaderAnimationScene from "app/scenes/FragShaderAnimationScene";
-import { ITextStyle } from "@pixi/text";
+import {ITextStyle} from "@pixi/text";
 import FontScene from "app/scenes/FontScene";
-import { inject } from "app/model/injection/InjectDecorator";
+import {inject} from "app/model/injection/InjectDecorator";
 import LayoutScene from "app/scenes/LayoutScene";
 import LayoutManager from "app/layoutManager/LayoutManager";
-import { PivotType } from "app/controls/MainControl";
+import {PivotType} from "app/controls/MainControl";
 import choiceSceneLayout from "app/scenes/ChoiceScene.layout";
 import SpineControlScene from "app/scenes/SpineControlScene";
 import GameScene from "app/scenes/GameScene";
@@ -33,22 +33,23 @@ export default class ChoiceScene extends BaseScene {
     private style:Partial<ITextStyle> = {
         fontFamily: "Arial",
         letterSpacing: 2,
-        fontSize: 24
+        fontSize: 24,
+        align: "center",
     };
     protected buttons = [
-        new TextButtonControl("Game Example", {target: GameScene, style: this.style, align:PivotType.TL}).name("item1_1"),
-        new TextButtonControl("Spine scene", {target: SpineScene, style: this.style, align:PivotType.TL}).name("item1_2"),
-        new TextButtonControl("Spine control scene", {target: SpineControlScene, style: this.style, align:PivotType.TL}).name("item2_1"),
-        new TextButtonControl("Shader scene", {target: ShaderAnimationScene, style: this.style, align:PivotType.TL}).name("item2_2"),
-        new TextButtonControl("Frag Shader scene", {target: FragShaderAnimationScene, style: this.style, align:PivotType.TL}).name("item3_1"),
-        new TextButtonControl("Font Scene", {target: FontScene, style: this.style, align:PivotType.TL}).name("item3_2"),
-        new TextButtonControl("Layout Scene", {target: LayoutScene, style: this.style, align:PivotType.TL}).name("item4_1"),
-        new TextButtonControl("Dev animation scene", {target: DevAnimationScene, style: this.style, align:PivotType.TL}).name("item4_2"),
-        new TextButtonControl("Gsap animation scene", {target: GsapDevAnimationScene, style: this.style, align:PivotType.TL}).name("item4_3"),
+        new TextButtonControl("Game\nExample", {target: GameScene, style: this.style, align:PivotType.TL}).name("item1_1"),
+        new TextButtonControl("Spine\nscene", {target: SpineScene, style: this.style, align:PivotType.TL}).name("item1_2"),
+        new TextButtonControl("Spine\ncontrol\nscene", {target: SpineControlScene, style: this.style, align:PivotType.TL}).name("item2_1"),
+        new TextButtonControl("Shader\nscene", {target: ShaderAnimationScene, style: this.style, align:PivotType.TL}).name("item2_2"),
+        new TextButtonControl("FragShader\nscene", {target: FragShaderAnimationScene, style: this.style, align:PivotType.TL}).name("item3_1"),
+        new TextButtonControl("Font\nScene", {target: FontScene, style: this.style, align:PivotType.TL}).name("item3_2"),
+        new TextButtonControl("Layout\nScene", {target: LayoutScene, style: this.style, align:PivotType.TL}).name("item4_1"),
+        new TextButtonControl("Dev\nanimation\nscene", {target: DevAnimationScene, style: this.style, align:PivotType.TL}).name("item4_2"),
+        new TextButtonControl("Gsap\nanimation\nscene", {target: GsapDevAnimationScene, style: this.style, align:PivotType.TL}).name("item4_3"),
         new TextButtonControl("OS_Pacman", {target: OS_GameScene, style: this.style, align:PivotType.TL}).name("item5_1"),
         new TextButtonControl("MS_Pacman", {target: GsapDevAnimationScene, style: this.style, align:PivotType.TL}).name("item5_2"),
         new TextButtonControl("GPacman", {target: GPacmanScene, style: this.style,align: PivotType.TL}).name("item5_3"),
-        new TextButtonControl("Spine symbols", {target: SymbolsScene, style: this.style, align:PivotType.TL}).name("item6_1"),
+        new TextButtonControl("Spine\nsymbols", {target: SymbolsScene, style: this.style, align:PivotType.TL}).name("item6_1"),
     ];
 
     compose():void {
