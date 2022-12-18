962d6706769f235c26a017b746fb8c8223ded9d3
Artem Panasuik <artriff.panasiuk@gmail.com>
2022-12-18 20:35:40 +0200

    add background image

--- pixiv6/src/scenes/SlotMachineScene.ts
+++ pixiv6/src/scenes/SlotMachineScene.ts
@@ -1,10 +1,14 @@
 import BaseScene from "app/scenes/BaseScene";
+import BackgroundControl from "app/controls/BackgroundControl";
+import Resources from "app/pixi/StrictResourcesHelper";
 import ReelControl from "app/controls/SlotMachine/ReelControl";
 
 export default class SlotMachineScene extends BaseScene {
-    private readonly reel = new ReelControl(16, 300);
+    private readonly background = new BackgroundControl(Resources.getSingleTexture("SLOT-MASHINE_BG"));
+    private readonly reel = new ReelControl(16, 300, 0);
 
     compose() {
+        this.addControl(this.background);
         this.addControl(this.reel);
     }
 }
