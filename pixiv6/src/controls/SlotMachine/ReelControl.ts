e8588db882e3429df2e8ccc83ed3ad6f3e8c85bc
alexsen-official <alexsen.official@gmail.com>
2022-12-20 02:57:03 +0200

    #22 | refactoring

--- pixiv6/src/controls/SlotMachine/ReelControl.ts
+++ pixiv6/src/controls/SlotMachine/ReelControl.ts
@@ -2,27 +2,26 @@ import MainControl from "app/controls/MainControl";
 import SymbolControl from "app/controls/SlotMachine/SymbolControl";
 
 export default class ReelControl extends MainControl {
-    private readonly symbols: SymbolControl[];
-    private readonly width: number;
-    private readonly height: number;
+    private readonly symbols: SymbolControl[] = [];
 
-    constructor(symbolsCount: number, width: number) {
+    constructor(
+        private readonly symbolsCount: number,
+        private readonly width: number,
+        private readonly offsetX: number,
+    ) {
         super();
 
-        this.symbols = new Array<SymbolControl>(symbolsCount);
+        this.container.x = width / 2 + width * offsetX;
 
-        this.width = width;
-        this.height = width * symbolsCount;
-
-        this.generateReel();
+        this.generateSymbols();
     }
 
-    private generateReel() {
-        for (let i = 0; i < this.symbols.length; i++) {
-            const symbol = new SymbolControl(this.width, this.width * i, this.height);
+    private generateSymbols() {
+        for (let i = 0; i < this.symbolsCount; i++) {
+            const symbol = new SymbolControl(this.width, i);
 
-            this.symbols[i] = symbol;
             this.add(symbol);
+            this.symbols.push(symbol);
         }
     }
 }
