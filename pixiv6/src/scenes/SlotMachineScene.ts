cc9a43eff1d07a3fb85307a0675aaa3218fe8850
alexsen-official <alexsen.official@gmail.com>
2022-12-20 02:59:41 +0200

    #22 | refactoring

--- pixiv6/src/scenes/SlotMachineScene.ts
+++ pixiv6/src/scenes/SlotMachineScene.ts
@@ -1,14 +1,10 @@
 import BaseScene from "app/scenes/BaseScene";
 import BackgroundControl from "app/controls/BackgroundControl";
 import Resources from "app/pixi/StrictResourcesHelper";
-import ReelControl from "app/controls/SlotMachine/ReelControl";
 
 export default class SlotMachineScene extends BaseScene {
     private readonly background = new BackgroundControl(Resources.getSingleTexture("SLOT-MASHINE_BG"));
-    private readonly reel = new ReelControl(16, 300, 0);
-
     compose() {
         this.addControl(this.background);
-        this.addControl(this.reel);
     }
 }
