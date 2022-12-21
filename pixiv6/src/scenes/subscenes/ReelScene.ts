bd61204684224ac613ab854ff7a1f28e85d570cb
hlib.savenko <hlib.savenko@sigma.software>
2022-12-21 02:45:37 +0200

    #17 | [Control] added uid for each controls;

--- pixiv6/src/scenes/subscenes/ReelScene.ts
+++ pixiv6/src/scenes/subscenes/ReelScene.ts
@@ -9,7 +9,8 @@ export default class ReelScene extends BaseScene {
 
     private reelBgControl = new SpriteControl("reels.png", {x: 0.5, y: 0.5});
 
-    compose(): void {
+    async compose() {
+        await gameModel.ready;
         this.addControl(this.reelBgControl);
         let mainGameInfo = gameModel.mainGameInfo;
         let reels = mainGameInfo.reels;
@@ -17,7 +18,7 @@ export default class ReelScene extends BaseScene {
         let reelHeight = bg.height;
         let reelWidth = bg.width;
         for (let i = 0; i < reels.amount; i++) {
-            let reelControl = new ReelControl(mainGameInfo.strips);
+            let reelControl = new ReelControl(i, mainGameInfo.strips[i]);
             reelControl.name(`reel_${i}`);
             this.reels.push(reelControl);
             this.reelBgControl.add(reelControl);
@@ -27,6 +28,7 @@ export default class ReelScene extends BaseScene {
         gameModel.game.signals.reels.updateSkin.add(this.onUpdateSkins, this);
         gameModel.game.signals.reels.updateAnimation.add(this.onUpdateAnimations, this);
         gameModel.game.signals.reels.spin.add(this.onSpin, this);
+        gameModel.game.signals.reels.stop.add(this.onStop, this);
     }
 
     protected onResize(gameSize: GameSize) {
@@ -50,6 +52,14 @@ export default class ReelScene extends BaseScene {
         await Promise.all(this.reels.map(async (reel, index) => {
             await promiseDelay((0.1 * index) * 1000);
             await reel.spin();
+        }));
+        gameModel.game.signals.spinStarted.emit();
+    }
+
+    private async onStop(reelStops:number[]) {
+        await Promise.all(this.reels.map(async (reel, index) => {
+            await promiseDelay((0.1 * index) * 1000);
+            await reel.stop(reelStops[index]);
         }));
         gameModel.game.signals.spinComplete.emit();
     }
