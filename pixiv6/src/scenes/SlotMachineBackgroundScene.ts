50814fbf1ba3c99214d81fcc709451e558cd84a0
hlib.savenko <hlib.savenko@sigma.software>
2022-12-22 14:20:50 +0200

    #26 | added more typings;

--- pixiv6/src/scenes/SlotMachineBackgroundScene.ts
+++ pixiv6/src/scenes/SlotMachineBackgroundScene.ts
@@ -1,14 +1,14 @@
 import BaseScene from "app/scenes/BaseScene";
 import BackgroundControl from "app/controls/BackgroundControl";
 import Resources from "app/pixi/StrictResourcesHelper";
-import { SpriteControl } from "app/controls/SpriteControl";
-import StrictResourcesHelper from "app/pixi/StrictResourcesHelper";
+import {SpriteControl} from "app/controls/SpriteControl";
 
 export default class SlotMachineBackgroundScene extends BaseScene {
-    private background = new BackgroundControl(Resources.getSingleTexture("SLOT-MASHINE_BG"));
-    private logo: any;
+
+    private readonly background: BackgroundControl = new BackgroundControl(Resources.getSingleTexture("SLOT-MASHINE_BG"));
+    private readonly logo: SpriteControl = new SpriteControl("logo_vertical.png", {x: -1, y: -0.5});
+
     compose(): void {
-        this.logo =  new SpriteControl("logo_vertical.png",{x:-1,y:-0.5});
         this.addControl(this.background);
         this.addControl(this.logo);
     }
